f3618d28ed96a168b78b395e46e1848cf6a5d30b
Qorvano <315753177+Qorvano@users.noreply.github.com>
2026-09-07 11:54:30 +0200

    Release 0.15.1

--- custom_components/kustos_vision/frontend/dist/panel.js
+++ custom_components/kustos_vision/frontend/dist/panel.js
@@ -1961,7 +1961,7 @@ async function ne() {
       s.discard();
   return !0;
 }
-const Pe = "0.15.0", xs = "kustos-vision-built:0.15.0", _s = {
+const Pe = "0.15.1", xs = "kustos-vision-built:0.15.1", _s = {
   ptz_up: "Schwenken hoch",
   ptz_down: "Schwenken runter",
   ptz_left: "Schwenken links",
